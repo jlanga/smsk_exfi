50b4e8dc75b8ce5ac6a0389da9d078ded9f931ac
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-12-03 14:19:29 +0100

    Softer trimmomatic. Bugfix in clean.py

--- src/snakefiles/clean.py
+++ src/snakefiles/clean.py
@@ -1,22 +1,4 @@
-rule clean_map:
-    """Just remove mappings"""
-    shell:
-        "rm -rf {MAP}"
-
-
-rule clean_call:
-    """Just remove the SNP calling"""
-    shell:
-        "rm -rf {CALL}"
-
-
-rule clean_report:
-    """Just remove the report"""
-    shell:
-        "rm -rf {REPORT_CALL}"
-
-
 rule clean:
     """Delete everything"""
     shell:
-        "rm -rf {MAP} {CALL} {REPORT_CALL}"
+        "rm -rf {RAW} {QC} {EXFI}"
